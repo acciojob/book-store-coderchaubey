358c53d3d7d939dad51648ca638c2f24e9afab23
coderchaubey <sunnychaubeysm@gmail.com>
2023-02-03 19:59:49 +0530

    solution

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -3,10 +3,7 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
@@ -19,5 +16,32 @@ public class BookController {
     public ResponseEntity createBook(@RequestBody Book book){
         Book newbook = bookService.createBook(book);
         return new ResponseEntity<>(newbook, HttpStatus.CREATED);
+    }
+    @GetMapping("/get-book-by-id/{id}")
+    public ResponseEntity findBookById(@PathVariable("id") int id){
+        return new ResponseEntity<>(bookService.findBookById(id),HttpStatus.FOUND);
+    }
+    @GetMapping("/get-all-books")
+    public ResponseEntity findAllBooks(){
+        return new ResponseEntity<>(bookService.findAllBooks(),HttpStatus.FOUND);
+    }
+    @GetMapping("/get-books-by-author")
+    public ResponseEntity findBooksByAuthor(@RequestParam("author")String author){
+
+        return new ResponseEntity<>(bookService.findBooksByAuthor(author), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/get-books-by-genre")
+    public ResponseEntity findBooksByGenre(@RequestParam("genre") String genre){
+        return new ResponseEntity<>(bookService.findBooksByGenre(genre),HttpStatus.FOUND);
+    }
+    @DeleteMapping("/delete-book-by-id/{id}")
+    public ResponseEntity deleteBookById(@PathVariable("id") int id){
+
+        return new ResponseEntity<>(bookService.deleteBookById(id),HttpStatus.ACCEPTED);
+    }
+    @DeleteMapping("/delete-all-books")
+    public void deleteAllBooks(){
+        bookService.deleteAllBooks();
     }
 }
--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -6,36 +6,70 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BookRepository {
 
+    private int id=1;
+    Map<Integer,Book> bookMap=new HashMap<>();
     public BookRepository(){
         
     }
 
     public Book save(Book book){
-        return null;
+        book.setId(id);
+        bookMap.put(book.getId(), book);
+        id++;
+        return book;
     }
 
     public Book findBookById(int id){
 
+        if (bookMap.containsKey(id)){
+            return bookMap.get(id);
+        }
         return null;
     }
 
     public List<Book> findAll(){
-        return null;
-    }
-
-    public void deleteBookById(int id){
-        return;
+        List<Book> books=new ArrayList<>();
+        for (Book book: bookMap.values()){
+            books.add(book);
+        }
+        return books;
     }
 
     public void deleteAll(){
+//        for (Integer i:bookMap.keySet()){
+//            bookMap.remove(i);
+//        }
+        bookMap.clear();
+        System.out.println("Deleted Successfully");
+
         return;
     }
 
     public List<Book> findBooksByAuthor(String author){
-        return null;
+        List<Book> books=new ArrayList<>();
+        for (Book book:bookMap.values()){
+            if (book.getAuthor().equals(author)){
+                books.add(book);
+            }
+        }
+        return books;
     }
 
     public List<Book> findBooksByGenre(String genre){
-        return null;
+        List<Book> books=new ArrayList<>();
+        for (Book book:bookMap.values()){
+            if (book.getGenre().equals(genre)){
+                books.add(book);
+            }
+        }
+        return books;
     }
+    public String deleteBookById(int id){
+        if(bookMap.containsKey(id)){
+            bookMap.remove(id);
+            return "Deleted Successfully";
+        }
+        return "Not Found";
+    }
+
 }
